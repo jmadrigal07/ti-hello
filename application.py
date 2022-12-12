ddfc082ae5c5b4f5a7f5ffbcb01f078cb8ad8224
Jorge Madrigal <jorge.madrigal@inmetrics.cl>
2022-12-12 13:00:06 -0500

    fix: change app content

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    return "Hello! I am the container: {}\n".format(socket.gethostname())
+    return "Hello again! I am the container: {}\n".format(socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5051)
